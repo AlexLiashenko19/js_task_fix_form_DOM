eb8b9e8721b24b19560d250198c454abe7316918
Александр Ляшенко <sanko.lialko14@gmail.com>
2025-05-19 16:51:32 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll('form input');
+
+  inputs.forEach((input, index) => {
+    const label = document.createElement('label');
+    label.classList.add('field-label');
+
+    if (!input.id) {
+      input.id = `input-${index}`;
+    }
+
+    label.setAttribute('for', input.id);
+    label.textContent = capitalize(input.name);
+
+    input.placeholder = capitalize(input.name);
+
+    input.parentElement.insertBefore(label, input);
+  });
+
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+});
